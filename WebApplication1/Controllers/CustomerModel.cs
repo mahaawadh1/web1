d4de08f11fe73fd536638bf5be4b04690e3d4bcf
mahaawadh1 <mahaaaq897@gmail.com>
2024-04-16 11:26:22 +0300

    web 1

---
+++ WebApplication1/Controllers/CustomerModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            List<Customer> customers = new List<Customer>()
+        {
+            new Customer { Id = 1, Name = "maha" },
+            new Customer { Id = 2, Name = " salem" },
+            new Customer { Id = 3, Name = "amar" }
+        };
+            ViewData["data"] = customers;
+            return View();
+        }
+    }
+}
